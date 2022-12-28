0c27f4dde4ce7b10a4a4e7419158811ffc54c171
kwon <kwon@DESKTOP-C1IV1RB>
2022-12-29 08:14:46 +0900

    test

---
+++ test/src/test/Ex1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Ex1 {
+
+	public static void main(String[] ast) {
+		String id = ast[0];
+		String pw = ast[1];
+		
+		System.out.println("id : " + id);
+		System.out.println("pw : " + pw);
+	}
+}
